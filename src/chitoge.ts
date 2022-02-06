1bac1267dad1278185c23f6186a3b0beb47e24e5
Dasabae <85019441+iamherok@users.noreply.github.com>
2022-02-06 13:30:47 +0530

    Update chitoge.ts

--- src/chitoge.ts
+++ src/chitoge.ts
@@ -22,7 +22,7 @@ const client = new WAClient({
 		.map((number) => `${number}@s.whatsapp.net`),
 	gkey: process.env.GOOGLE_API_KEY || "",
         hapi: process.env.HEROKU_API || "",
-        hname: process.env.HEROKU_DYNO-NAME || "",
+        hname: process.env.HEROKU_DYNO_NAME || "",
 	chatBotUrl: process.env.CHAT_BOT_URL || "",
 	gifApi: process.env.TENOR_API_KEY || "",
 	geniusKey: process.env.GENIUS_KEY || "",
